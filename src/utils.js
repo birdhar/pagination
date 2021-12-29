746b32a5bc00da95ef951060f79b32711980b49e
Birat Dhar <birat.dhar.89@gmail.com>
2021-12-29 07:36:31 +0530

    'commit'

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+const paginate = (followers) => {
+    const itemsPerPage = 10;
+    const pages = Math.ceil(followers.length / itemsPerPage) 
+    
+    const newFollowers = Array.from({length:pages}, (_,index)=>{
+        const followersPerPage = index * itemsPerPage
+        
+        return followers.slice(followersPerPage, followersPerPage + itemsPerPage)
+    })
+
+    return newFollowers
+}
+
+export default paginate
